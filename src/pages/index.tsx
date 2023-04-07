8e0c5282abdd0c8d8823968d8421104665394fb2
Taranpreet Singh <taranpreet.handysolver@gmail.com>
2023-04-08 00:24:13 +0530

    FIxing vercel errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -181,7 +181,12 @@ const Home: NextPage = () => {
               </div>
 
               <div className="flex w-full justify-between">
-                <Button buttonText={"Continue"} />
+                <button
+                  type="button"
+                  className="aspect-2/1 w-150  rounded-md border border-solid bg-transparent px-5 py-2.5 text-lg text-white transition-all duration-200 ease-in-out hover:cursor-pointer hover:bg-white hover:text-black"
+                >
+                  Continue
+                </button>
 
                 <SignOutButton>
                   <button
